12f3b5dac1f6cd80f67c1ac4b311f00640bf48b3
paulopez78 <pau.lopez.78@gmail.com>
2016-01-16 20:01:37 +0100

    fix

--- src/main/java/morse/MorseDecoder.java
+++ src/main/java/morse/MorseDecoder.java
@@ -7,29 +7,26 @@
  import java.util.*;
 
  public class MorseDecoder {
-
      private static String text = ".--..-..-.-.-----.-----....--...-.-.-..-....--.-......----.";
+     private static Map<String,String> codes;
+     private static List<String> dictionary;
 
-    private static Map<String,String> codes;
-    private static List<String> dictionary;
-    public static void main(String[] args) throws IOException
-    {
-        codes = createCodes();
-        dictionary = readDictionary("/users/pau/morse/src/main/resources/dictionary.txt");
-
-        generateItem(0,1,"","");
-    }
+     public static void main(String[] args) throws IOException {
+         codes = createCodes();
+         dictionary = readDictionary("/users/pau/morse/src/main/resources/dictionary.txt");
+         generateItem(0, 1, "", "");
+     }
 
     private static void generateItem(int index, int length, String word, String sentence) {
-        boolean outOfBounds = index + length > text.length();
-
         if (dictionary.contains(word)) {
             sentence = sentence + " " + word;
             word = "";
         }
 
+        boolean outOfBounds = index + length > text.length();
         if (outOfBounds || !anyWordStartsWith(word)) {
-            if (outOfBounds){
+            if (outOfBounds)
+            {
                 System.out.println(sentence);
             }
             return;
@@ -37,10 +34,8 @@
         else
         {
             String code = text.substring(index, index + length);
-            if (codes.containsKey(code))
-            {
-                String newWord = word.concat(codes.get(code).toLowerCase());
-                generateItem(index + length, 1, newWord, sentence);
+            if (codes.containsKey(code)) {
+                generateItem(index + length, 1, word.concat(codes.get(code)), sentence);
                 generateItem(index, length + 1, word, sentence);
             }
         }
@@ -59,32 +54,32 @@
     private static Map<String,String> createCodes()
     {
         Map<String,String> codes = new HashMap<>();
-        codes.put(".-","A");
-        codes.put("-...","B");
-        codes.put("-.-.","C");
-        codes.put("-..","D");
-        codes.put(".","E");
-        codes.put("..-.","F");
-        codes.put("--.","G");
-        codes.put("....","H");
-        codes.put("..","I");
-        codes.put(".---","J");
-        codes.put("-.-","K");
-        codes.put(".-..","L");
-        codes.put("--","M");
-        codes.put("-.","N");
-        codes.put("---","O");
-        codes.put(".--.","P");
-        codes.put("--.-","Q");
-        codes.put(".-.","R");
-        codes.put("...","S");
-        codes.put("-","T");
-        codes.put("..-","U");
-        codes.put("...-","V");
-        codes.put(".--","W");
-        codes.put("-..-","X");
-        codes.put("-.--","Y");
-        codes.put("--..","Z");
+        codes.put(".-","a");
+        codes.put("-...","b");
+        codes.put("-.-.","c");
+        codes.put("-..","d");
+        codes.put(".","e");
+        codes.put("..-.","f");
+        codes.put("--.","g");
+        codes.put("....","h");
+        codes.put("..","i");
+        codes.put(".---","j");
+        codes.put("-.-","k");
+        codes.put(".-..","l");
+        codes.put("--","m");
+        codes.put("-.","n");
+        codes.put("---","o");
+        codes.put(".--.","p");
+        codes.put("--.-","q");
+        codes.put(".-.","r");
+        codes.put("...","s");
+        codes.put("-","t");
+        codes.put("..-","u");
+        codes.put("...-","v");
+        codes.put(".--","w");
+        codes.put("-..-","x");
+        codes.put("-.--","y");
+        codes.put("--..","z");
         return codes;
     }
 }
